aeea351ebed73f8136107c74878b64ff7ecc15ed
RickyAnchores <ricky23anchores@gmail.com>
2023-11-22 20:08:04 +0000

    edit main

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='Home'>
+        <h1>Home</h1>
+        <p>This home</p>
+    </div>
+  )
+}
+
+export default Home
